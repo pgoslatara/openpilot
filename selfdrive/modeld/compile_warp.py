d5dbf4c2bf21ea3d5a0786231f56a4515d590723
Bruce Wayne <harald.the.engineer@gmail.com>
2025-11-14 10:26:47 -0800

    compile test script

---
+++ selfdrive/modeld/compile_warp.py
@@ -0,0 +1,53 @@
+import time
+from tinygrad.tensor import Tensor
+
+
+MODEL_WIDTH = 512
+MODEL_HEIGHT = 256
+MODEL_FRAME_SIZE = MODEL_WIDTH * MODEL_HEIGHT * 3 // 2
+IMG_INPUT_SHAPE = (1, 12, 128, 256)
+
+
+def tensor_arange(end):
+    return Tensor([float(i) for i in range(end)])
+
+def tensor_round(tensor):
+    return (tensor + 0.5).floor()
+
+
+h_src, w_src = 1208, 1928
+h_dst, w_dst = MODEL_HEIGHT, MODEL_WIDTH
+x = tensor_arange(w_dst).reshape(1, w_dst).expand(h_dst, w_dst)
+y = tensor_arange(h_dst).reshape(h_dst, 1).expand(h_dst, w_dst)
+ones = Tensor.ones_like(x)
+dst_coords = x.reshape((1,-1)).cat(y.reshape((1,-1))).cat(ones.reshape((1,-1)))
+
+def warp_perspective_tinygrad(src, M_inv):
+  src_coords = M_inv @ dst_coords
+  src_coords = src_coords / src_coords[2:3, :]
+
+  x_src = src_coords[0].reshape(h_dst, w_dst)
+  y_src = src_coords[1].reshape(h_dst, w_dst)
+
+  x_nearest = tensor_round(x_src).clip(0, w_src - 1).cast('int')
+  y_nearest = tensor_round(y_src).clip(0, h_src - 1).cast('int')
+
+  dst = src[y_nearest, x_nearest]
+  return dst
+
+
+if __name__ == "__main__":
+  from tinygrad.engine.jit import TinyJit
+  update_img_jit = TinyJit(warp_perspective_tinygrad, prune=True)
+
+  inputs = [Tensor.randn(1928,1208).realize(), Tensor.randn(3,3).realize()]
+  # run 20 times
+  step_times = []
+  for _ in range(20):
+    st = time.perf_counter()
+    out = update_img_jit(*inputs)
+    mt = time.perf_counter()
+    val = out.realize()
+    et = time.perf_counter()
+    step_times.append((et-st)*1e3)
+    print(f"enqueue {(mt-st)*1e3:6.2f} ms -- total run {step_times[-1]:6.2f} ms")
